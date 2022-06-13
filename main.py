30ffb9555699c9a859d064e8d03de2186b882675
Teeyanah-K <105463121+Teeyanah-K@users.noreply.github.com>
2022-06-13 15:53:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import random
-options = ['R', 'P', 'S']
-print("Let's play Rock, Paper, Scissors...\n 'R' for Rock\n 'P' for Paper\n 'S' for Scissors.")
 
-rand = random.choice(options)
-Player = input("input R,P or S: ")
-R = "rock"
-P = "paper"
-S = "paper"
 
-print("CPU:", rand)
-print("Player:", Player)
+while True:
+    options = ['R', 'P', 'S']
+    computer = random.choice(options)
+    player = input("Enter move:")
 
-Player = input("input R,P or S: ")
-while Player not in options:
-    while rand != Player:
-        if rand == R and Player == S:
-            print("CPU wins")
-        elif rand == R and Player == P:
-            print("You won!")
-        elif rand == P and Player == R:
-            print("CPU wins")
-        elif rand == P and Player == S:
-            print("You won!")
-        elif rand == S and Player == P:
-            print("CPU wins")
-        elif:
-            print("You won!")
+    while player not in options:
+        print('Invalid input!')
+        player = input("Pick an option from R, P and S:")
+
+    print(f"The computer plays: {computer}")
+
+    while player == computer:
+        print("It's a tie!")
+        player = input("Pick an option from R, P and S:")
+
+    if player == "R":
+        if computer == "P":
+            print("Computer wins :( ")
         else:
-            print("It's a tie. Try again!")   
-    print("Error! Invalid Input")
+            print("Player wins!")
+    elif player == "P":
+        if computer == "R":
+            print("Player win!")
+        else:
+            print("Computer wins!")
+    elif (player == "S"):
+        if (computer == "R"):
+            print("Computer wins!")
+        else:
+            print("You win!")
+    break
+    
